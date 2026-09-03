1b4b7d028522c4fdf0c6094c3e82a27bfb5ab08b
Hermilson Delgado <hdelgadoh@indracompany.com>
2026-09-03 16:14:18 -0500

    test: inicio

--- sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Bus.java
+++ sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Bus.java
@@ -1,10 +1,12 @@
 package com.indra.transporte.model;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
+@RequiredArgsConstructor
 public class Bus {
     private final String placa;
     private final String tipo;
